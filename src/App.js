3ea90517ab094748aeeb20694216ba956ecd1688
AnarMikailov <a.v.mikayilov@gmail.com>
2023-03-14 16:47:16 +0400

    adding folder sturucture

--- src/App.js
+++ src/App.js
@@ -26,8 +26,42 @@ const App = () => {
             SideBar
           </div>
         ) : (
-          <div>Sidebar w-0</div>
+          <div className="w-0 dark:bg-secondar-dark-bg">Sidebar </div>
         )}
+        <div
+          className={`dark:bg-main-bg bg-main-bg min-h-dvreen  w-full ${
+            activeMenu ? " md:ml-72" : "flex-2 "
+          }`}
+        >
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full"></div>
+        </div>
+        <div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path="/" element="Ecommerce" />
+            <Route path="/ecommerce" element="Ecommerce" />
+            {/* Pages */}
+            <Route path="/orders" element="Orders" />
+            <Route path="/employees" element="Employees" />
+            <Route path="/customers" element="Customers" />
+            <Route path="/orders" element="Orders" />
+
+            {/* Pages */}
+            <Route path="/kanban" element="Kanban" />
+            <Route path="/editor" element="Editor" />
+            <Route path="/calendar" element="Calendar" />
+            <Route path="/color-picker" element="ColorPicker" />
+            {/* Charts */}
+            <Route path="/line" element="Line" />
+            <Route path="/area" element="Area" />
+            <Route path="/bar" element="Bar" />
+            <Route path="/pie" element="Pie" />
+            <Route path="/financial" element="Financial" />
+            <Route path="/color-mapping" element="ColorMapping" />
+            <Route path="/pyramid" element="Pyramid" />
+            <Route path="/Stacked" element="Stacked" />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
